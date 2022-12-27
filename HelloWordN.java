d3a69937c77b7590ef36bb16a7436a05dc92d915
unknown <adityamadkar20@gmail.com>
2022-12-27 18:07:25 +0530

    first commit

---
+++ HelloWordN.java
@@ -0,0 +1,19 @@
+
+
+public class HelloWordN{
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		char c='H';
+		double d=20.22;
+		
+	
+		
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
+# GitHubJavaProjectRepository
